18058ec89df4046d4d42645d40882d977c91b3f1
AMS <AMS@SVALKER-PC>
2023-05-22 23:46:18 +0300

    Добавьте файлы проекта.

---
+++ MinHeap.cs
@@ -0,0 +1,148 @@
+﻿public class MinHeap
+{
+    public int[] heap;
+    private int size;
+    public MinHeap(int capacity)
+    {
+        heap = new int[capacity];
+        size = 0;
+    }
+
+    public int Parent(int index)
+    {
+        return (index - 1) / 2;
+    }
+
+    public int[] Children(int index)
+    {
+        int leftChild = 2 * index + 1;
+        int rightChild = 2 * index + 2;
+        int[] children = new int[2];
+
+        if (leftChild < size)
+            children[0] = heap[leftChild];
+        else
+            children[0] = -1;
+
+        if (rightChild < size)
+            children[1] = heap[rightChild];
+        else
+            children[1] = -1;
+
+        return children;
+    }
+
+    private void SiftUp(int index)
+    {
+        int parentIndex = Parent(index);
+
+        while (index > 0 && heap[index] < heap[parentIndex])
+        {
+            Swap(index, parentIndex);
+            index = parentIndex;
+            parentIndex = Parent(index);
+        }
+    }
+
+    private void SiftDown(int index)
+    {
+        int minChildIndex = GetMinChildIndex(index);
+
+        while (minChildIndex != -1 && heap[index] > heap[minChildIndex])
+        {
+            Swap(index, minChildIndex);
+            index = minChildIndex;
+            minChildIndex = GetMinChildIndex(index);
+        }
+    }
+
+    private int GetMinChildIndex(int index)
+    {
+        int leftChild = 2 * index + 1;
+        int rightChild = 2 * index + 2;
+
+        if (leftChild >= size)
+            return -1;
+
+        if (rightChild >= size)
+            return leftChild;
+
+        return heap[leftChild] < heap[rightChild] ? leftChild : rightChild;
+    }
+
+    public void BuildHeap(int[] array)
+    {
+        if (array.Length > heap.Length)
+            throw new ArgumentException("Емкость превышена");
+
+        heap = array;
+        size = array.Length;
+
+        for (int i = Parent(size - 1); i >= 0; i--)
+        {
+            SiftDown(i);
+        }
+    }
+
+    public int ExtractMin()
+    {
+        if (size == 0)
+            throw new InvalidOperationException("Куча пустая");
+
+        int min = heap[0];
+        heap[0] = heap[size - 1];
+        size--;
+        SiftDown(0);
+        return min;
+    }
+
+    public void Insert(int value)
+    {
+        if (size == heap.Length)
+            throw new InvalidOperationException("Куча полная");
+
+        heap[size] = value;
+        size++;
+        SiftUp(size - 1);
+    }
+
+    public int[] HeapSort()
+    {
+        int[] sortedArray = new int[size];
+        int heapSize = size;
+
+        for (int i = 0; i < heapSize; i++)
+        {
+            sortedArray[i] = ExtractMin();
+        }
+
+        return sortedArray;
+    }
+
+    private void Swap(int index1, int index2)
+    {
+        int temp = heap[index1];
+        heap[index1] = heap[index2];
+        heap[index2] = temp;
+    }
+
+    public override string ToString()
+    {
+        return string.Join(", ", heap[..size]);
+    }
+    public bool IsMinHeap()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            int[] children = Children(i);
+
+            if (children[0] != -1 && heap[i] < children[0])
+                return false;
+
+            if (children[1] != -1 && heap[i] < children[1])
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿//Задание 1
+//    string inputFilePath = "input.txt";
+//    int[] heapArray = ReadHeapArrayFromFile(inputFilePath);
+
+//    MinHeap heap = new MinHeap(heapArray.Length);
+//    heap.BuildHeap(heapArray);
+
+//    bool isMaxHeap = IsMaxHeap(heapArray.Length, heap);
+//    string result = isMaxHeap ? "YES" : "NO";
+//    Console.WriteLine(result);
+
+//static int[] ReadHeapArrayFromFile(string filePath)
+//{
+//    string[] lines = File.ReadAllLines(filePath);
+//    int[] heapArray = Array.ConvertAll(lines[1].Split(' '), int.Parse);
+//    return heapArray;
+//}
+
+//static bool IsMaxHeap(int size, MinHeap heap)
+//{
+//    for (int i = 0; i < size; i++)
+//    {
+//        int[] children = heap.Children(i);
+//        int leftChild = children[0];
+//        int rightChild = children[1];
+
+//        if (leftChild != -1 && heap.heap[i] < leftChild)
+//            return false;
+
+//        if (rightChild != -1 && heap.heap[i] < rightChild)
+//            return false;
+//    }
+
+//    return true;
+//}
+//Задание 2
+//string inputFilePath = "input.txt";
+//int[] array = ReadArrayFromFile(inputFilePath);
+
+//MinHeap heap = new MinHeap(array.Length);
+//heap.BuildHeap(array);
+
+//Console.WriteLine("Heap: " + heap.ToString());
+
+//    static int[] ReadArrayFromFile(string filePath)
+//{
+//    string[] lines = File.ReadAllLines(filePath);
+//    int size = int.Parse(lines[0]);
+//    int[] array = Array.ConvertAll(lines[1].Split(' '), int.Parse);
+//    return array;
+//}
+//Задание 3
+string inputFilePath = "input.txt";
+int[] array = ReadArrayFromFile(inputFilePath);
+
+MinHeap heap = new MinHeap(array.Length);
+heap.BuildHeap(array);
+
+int[] sortedArray = heap.HeapSort();
+Console.WriteLine("Sorted Array: " + string.Join(" ", sortedArray));
+
+    static int[] ReadArrayFromFile(string filePath)
+{
+    string[] lines = File.ReadAllLines(filePath);
+    int size = int.Parse(lines[0]);
+    int[] array = Array.ConvertAll(lines[1].Split(' '), int.Parse);
+    return array;
+}
